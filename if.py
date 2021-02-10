a14c37f1f8be4e7f6e683020ffc8ed9acbe8673a
CubeTechBiz <77924606+CubeTechBiz@users.noreply.github.com>
2021-02-10 15:03:24 +0530

    Break program

--- if.py
+++ if.py
@@ -1,11 +1,4 @@
-# If the number is positive, we print an appropriate message
-
-num = 3
+num = 5
 if num > 0:
-    print(num, "is a positive number.")
-print("This is always printed.")
-
-num = -1
-if num > 0:
-    print(num, "is a positive number.")
-print("This is also always printed.")
+    print(num,"is the biger number")
+    print("is alway be the bigger number")
