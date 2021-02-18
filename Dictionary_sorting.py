a747dff9fe38d56f6b69f6dbbcbd833235c9b052
CubeTechBiz <77924606+CubeTechBiz@users.noreply.github.com>
2021-02-18 15:35:28 +0530

    Dictionary sorting program

---
+++ Dictionary_sorting.py
@@ -0,0 +1,10 @@
+PlayerTitles = {
+   'Federer': 20,
+   'Nadal': 20,
+   'Djokovic': 17,
+   'Murray': 3,
+   'Theim' : 1,
+   'Zverev': 0
+}
+#type(PlayerTitles)
+print(f"Output \n*** The minimum value in the dictionary is {min(PlayerTitles)} ")
