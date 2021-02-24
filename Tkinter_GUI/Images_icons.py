75e66fc86f86337c770cc3da4b689702c2860d58
CubeTechBiz <77924606+CubeTechBiz@users.noreply.github.com>
2021-02-24 17:36:49 +0530

    Images and Icons

--- Tkinter_GUI/Images_icons.py
+++ Tkinter_GUI/Images_icons.py
@@ -1,12 +1,9 @@
 from tkinter import *
 
-from PIL import ImageTk, Image
-
 root = Tk()
-canvas = Canvas(root, width = 300, height = 160)
-image = ImageTk.PhotoImage(Image.open("C:\\Users\\CubeTechBiz\\Desktop\\NX 10 picture\\image.png"))
 
-canvas.create_image(0, 0, anchor = NW, image = image)
-canvas.pack()
+photo = PhotoImage(file = "sigma.png")
+label = Label(root, image = photo)
+label.pack()
 
 root.mainloop()
