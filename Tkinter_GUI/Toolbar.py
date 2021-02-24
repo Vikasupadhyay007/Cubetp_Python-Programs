be90616195bc0e0af2871a2c67546043563b5c63
CubeTechBiz <77924606+CubeTechBiz@users.noreply.github.com>
2021-02-24 14:32:19 +0530

    Creating a Toolbar

---
+++ Tkinter_GUI/Toolbar.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+def doNothing():
+    print("ok I got it")
+
+root = Tk()
+
+# **** main menu ****
+
+menu = Menu(root)
+root.config(menu = menu)
+
+subMenu = Menu(menu)
+menu.add_cascade(label = "File", menu = subMenu)
+subMenu.add_command(label = "New Project", command = doNothing)
+subMenu.add_command(label = "New", command = doNothing)
+subMenu.add_separator()
+subMenu.add_command(label = "Exit", command = doNothing)
+
+editMenu = Menu(menu)
+menu.add_cascade(label = "Edit", menu = editMenu)
+editMenu.add_command(label = "Redo", command = doNothing)
+
+# **** toolbar ****
+
+toolbar = Frame(root, bg = "blue")
+
+inserButt = Button(toolbar, text = "Insert image", command = doNothing)
+inserButt.pack(side = LEFT, padx = 2, pady = 2)
+printButt = Button(toolbar, text = "Print", command = doNothing)
+printButt.pack(side = LEFT, padx = 2, pady = 2)
+
+toolbar.pack(side = TOP, fill = X)
+
+root.mainloop()
