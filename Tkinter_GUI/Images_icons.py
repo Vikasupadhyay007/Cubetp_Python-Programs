789bfeb5719b9d99900e0ab318f0a8e3a76b0179
CubeTechBiz <77924606+CubeTechBiz@users.noreply.github.com>
2021-02-25 18:53:14 +0530

    Images and Icons

--- Tkinter_GUI/Images_icons.py
+++ Tkinter_GUI/Images_icons.py
@@ -1,9 +1,21 @@
 from tkinter import *
+from PIL import Image, ImageTk # in terminal type: pip install pillow
 
 root = Tk()
 
+# PNG picture import 
+
 photo = PhotoImage(file = "wavelet logo.png")
+
 label = Label(root, image = photo)
+label.pack()
+
+# JPG picture import
+
+#image = Image.open("B_01.jpg")
+#photo = ImageTk.PhotoImage(image)
+
+#label = Label(image = photo)
 label.pack()
 
 root.mainloop()
